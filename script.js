01cebe280f9a5f570908857424908bd29f410ccb
Luciany Laurentino <lucylaurentino@gmail.com>
2017-10-11 15:49:09 +1300

    finishingclasses

--- script.js
+++ script.js
@@ -5,57 +5,57 @@ let lname = document.querySelector('#lname')
 let salary = document.querySelector('#salary')
 
 let FirstName = document.querySelector('#FirstName')
-let submit = document.querySelector('#submit') 
+let submit = document.querySelector('#submit')
 
 
-{class Employee {
+class Employee {
 
     get fname() {
-    return '${this._fname}'
+        return `${this._fname}`
     }
-    set fname() {
-        this._fname = '${value}'
+    set fname(value) {
+        this._fname = `${value}`
     }
     get lname() {
-        return '${this._lname}'
+        return `${this._lname}`
     }
-    set lname() {
-        this._lname = '${value}'
+    set lname(value) {
+        this._lname = `${value}`
     }
     get salary() {
-        return '{this._salary}'
+        return `${this._salary}`
     }
-    set salary() {
-        this._salary = '${value}'
-    
-
-function constructor(fname, lname, salary){
-    this._fname = fname;
-    this._lname = lname;
-    this._salary = salary; 
-
-//Create a function to display input 
-//function displayEmployee(fname, lname, salary){
-//Employee.call(this, fname, lname, salary); //call class, base construction function
-    //console.log('Last Name', $(this,_lname));
-}
-for(var i = 1; i < table.rows.length; i++)
-{
-    table.rows[i].onclick = function(){
-        document.getElementById("fname"). value = this.cells[0]. innerHTML;
-        document.getElementById("lname"). value = this.cells[1]. innerHTML;
-        document.getElementById("salary"). value = this.cells[2]. innerHTML;
-        console.log('hi')
+    set salary(value) {
+        this._salary = `${value}`
     }
-}
 
-//displayEmployee.prototype = new Employee(); // create a prototype chain
-//displayEmployee.prototype.constructor = displayEmployee
-}
-//var lucy = new.Employee();
-//lucy.fname = "Lucy"
-//lucy.lname = "Laurentino"
-//lucy.salary = 1600.00
+    constructor(fname, lname, salary) {
+        this._fname = fname;
+        this._lname = lname;
+        this._salary = salary;
+    }
+
+
+    displayEmployee() {
+        Employee.call(this, fname, lname, salary); //call class, base construction function
+        
+    }
+
+        for (var i = 1; i < table.rows.length; i++) {
+            table.rows[i].onclick = function () {
+                document.getElementById("fname").value = this.cells[0].innerHTML;
+                document.getElementById("lname").value = this.cells[1].innerHTML;
+                document.getElementById("salary").value = this.cells[2].innerHTML;
+            }
+        }
+
+        //displayEmployee.prototype = new Employee(); // create a prototype chain
+        //displayEmployee.prototype.constructor = displayEmployee
+    }
+    //var lucy = new.Employee();
+    //lucy.fname = "Lucy"
+    //lucy.lname = "Laurentino"
+    //lucy.salary = 1600.00
 
 }
 }
